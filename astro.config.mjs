3dff634dea3e208f02cd374de2c233dca3996ef0
danielguarimatadeveloper <danigt.dev@gmail.com>
2025-12-20 21:07:24 -0400

    Restableciendo rutas del Path (cambiando por base: /daniel_portafolio)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/',
+  base: '/daniel_portafolio/',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
